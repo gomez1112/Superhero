87caa0900b2cddc091eb19440b8e7bf69b23b383
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2023-12-30 08:46:15 -0500

    Initial Commit

---
+++ Superhero/SuperheroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperheroApp.swift
+//  Superhero
+//
+//  Created by Gerard Gomez on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SuperheroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
